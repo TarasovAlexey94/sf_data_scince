86f1e9172f0b6f4225172505d19099a05b43b46c
alexey_tarasov <wishluck94@gmail.com>
2023-01-29 14:00:02 +0300

    edit commit

--- __pycache__/project_0/game_v3.py
+++ __pycache__/project_0/game_v3.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 random_number = random.randint(1, 101) #Загадывае слуайное число в выбраннном  диапазоне
 
-def game_v3(number:int=1)->int:
+def game_v3(number: int = 1) -> int:
     """Рандомно угадываем число
 
     Args:
@@ -15,8 +15,8 @@ def game_v3(number:int=1)->int:
     min = 1
     max = 100
     while True: #Запускаем цикл отбора
-        count+=1
-        mid = (min+max)//2 #Приеняем метод деленния на 2
+        count += 1
+        mid = (min + max)//2 #Приеняем метод деленния на 2
         if mid == random_number:
             break  #выход из цикла, если угадали
         elif mid > random_number:
@@ -25,7 +25,7 @@ def game_v3(number:int=1)->int:
             min = mid
     return (count) #Возвращем число попыток
 
-def score_game(game_v3)->int:
+def score_game(game_v3) -> int:
     """За какое количество попыток в среднем из 1000 подходов 
     угадывает наш алгоритм
 
@@ -36,9 +36,9 @@ def score_game(game_v3)->int:
     Returns:
         int: среднее количество попыток
     """
-    count_1s=[]   #список для сохранения количества попыток
+    count_1s = []   #список для сохранения количества попыток
     np.random.seed(1)   #фиксируем сид для воспроизводимости
-    random_array=np.random.randint(1, 101, size=(1000))  #загадали список чисел
+    random_array=np.random.randint(1, 101, size = (1000))  #загадали список чисел
     for number in random_array:
         count_1s.append(game_v3(number))
     score=int(np.mean(count_1s))  #находим среднее количество попыток
